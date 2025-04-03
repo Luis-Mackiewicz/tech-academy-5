6b1964aa474e550b72c1518155c09133d3a1fdce
Luis Miguel Mackiewicz <159212359+Luis-Mackiewicz@users.noreply.github.com>
2025-04-03 17:50:07 -0300

    adding layout and configuring routes

--- front-end/taskflow/src/components/Header.tsx
+++ front-end/taskflow/src/components/Header.tsx
@@ -1,51 +1,67 @@
+import { Link, useLocation } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+
 function Header() {
-  const currentPath = "/";
+  const location = useLocation();
 
   return (
-    <header className="bg-[#004345] h-20 w-full max-w-[1440px] mt-4 mx-auto px-4 md:px-8 rounded flex items-center">
+    <header className="bg-[#004345] h-[8vh] w-[90vw] mt-[4vh] mx-auto md:px-8 flex items-center">
       <nav className="w-full flex items-center justify-between">
         <div className="flex items-center">
           <div className="mr-8">
-            <span className="text-xl font-bold text-[#97D1D2]">TASKFLOW</span>
+            <span className="text-base md:text-lg lg:text-xl xl:text-2xl font-bold text-[#84b3b4]">
+              TASKFLOW
+            </span>
           </div>
 
           <ul className="flex gap-1 text-[#FFFFFF] font-medium">
             <li>
-              <a
-                href="/"
-                className={`block px-4 py-2 rounded-[4px] transition-colors duration-200 ${
-                  currentPath === "/"
+              <Link
+                to="/"
+                className={`px-4 py-2 rounded-[4px] transition-colors duration-200 ${
+                  location.pathname === "/"
                     ? "bg-[#f4fbfa] text-gray-900"
                     : "hover:bg-[#161d1d]"
                 }`}
               >
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/projetos"
-                className={`block px-4 py-2 rounded-[4px] transition-colors duration-200 ${
-                  currentPath === "/projetos"
+              <Link
+                to="/about"
+                className={`px-4 py-2 rounded-[4px] transition-colors duration-200 ${
+                  location.pathname === "/about"
+                    ? "bg-[#f4fbfa] text-gray-900"
+                    : "hover:bg-[#161d1d]"
+                }`}
+              >
+                Sobre
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/projects"
+                className={`px-4 py-2 rounded-[4px] transition-colors duration-200 ${
+                  location.pathname === "/projects"
                     ? "bg-[#f4fbfa] text-gray-900"
                     : "hover:bg-[#161d1d]"
                 }`}
               >
                 Projetos
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/config"
-                className={`block px-4 py-2 rounded-[4px] transition-colors duration-200 ${
-                  currentPath === "/config"
+              <Link
+                to="/settings"
+                className={`px-4 py-2 rounded-[4px] transition-colors duration-200 ${
+                  location.pathname === "/settings"
                     ? "bg-[#f4fbfa] text-gray-900"
                     : "hover:bg-[#161d1d]"
                 }`}
               >
                 Config
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
---
+++ front-end/taskflow/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <main className="flex">
+        <div className="justify-between pl-[5%]">
+          <h1 className="text-6xl text-[#97D1D2] font-bold">Taskflow</h1>
+          <h2>Organize e Gerencie seus proejtos de TI</h2>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
