658e8de43c8a012b602a23b7c4cfe1da9cf2d127
Luis Miguel Mackiewicz <159212359+Luis-Mackiewicz@users.noreply.github.com>
2025-04-03 19:36:24 -0300

    feat: inserting image on home page

--- front-end/taskflow/src/pages/Home.tsx
+++ front-end/taskflow/src/pages/Home.tsx
@@ -1,10 +1,25 @@
+import imageKanban from "../assets/imageKanban.svg";
+
 function Home() {
   return (
     <>
       <main className="flex">
-        <div className="justify-between pl-[5%]">
-          <h1 className="text-6xl text-[#97D1D2] font-bold">Taskflow</h1>
-          <h2>Organize e Gerencie seus proejtos de TI</h2>
+        <div className="justify-between pl-[5%] w-1/2">
+          <h1 className="text-[6.25rem] text-[#97D1D2] font-bold pt-[8%]">
+            Taskflow
+          </h1>
+          <h2 className="text-[3rem] text-[#161D1D] pt-[16%]">
+            Organize e Gerencie
+            <br /> Seus Projetos de TI com <br />
+            Facilidade e Eficiência
+          </h2>
+        </div>
+        <div className="w-1/2 flex items-center justify-center pr-[5%]">
+          <img
+            src={imageKanban}
+            alt="Image Kanban"
+            className="max-w-full max-h-[80vh] object-contain pt-[8%]"
+          />
         </div>
       </main>
     </>
