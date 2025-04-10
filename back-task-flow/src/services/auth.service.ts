4168dee54bf01e1e35fca64358f2b9f0ad4ed519
Luis Miguel Mackiewicz <159212359+Luis-Mackiewicz@users.noreply.github.com>
2025-04-10 00:52:58 -0300

    fix: update bcrypt package to bcryptjs and restore config.json

--- back-task-flow/src/services/auth.service.ts
+++ back-task-flow/src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { AppDataSource } from "../data-source";
 import { User } from "../entities/User";
