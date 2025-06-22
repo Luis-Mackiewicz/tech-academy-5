6c03c30dcc50b6d9b18a7bcba255a0689551dbca
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-22 17:33:05 -0300

    chore: removing folder to own repository

--- back-task-flow/src/migrations/1744280181581-FakeSync.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class FakeSync1744280181581 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
