924d5692b0a6ea454457285ebc91c8df6a5546dc
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-23 15:32:42 -0300

    feat(home): create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,18 @@ export default function Home() {
           <h1 className="text-white font-black text-4xl mb-6 text-center">
             Bem vindo ao Taskflow
           </h1>
-          <p className="text-white text-lg text-center max-w-md">
+          <p className="text-white text-lg text-center max-w-md font-medium">
             No mundo dinâmico de hoje, produtividade é mais do que fazer mais em
-            menos tempo — é sobre foco, organização e fluxo contínuo de
-            trabalho. O TaskFlow foi criado para transformar sua rotina com um
-            sistema visual de Kanban intuitivo, flexível e eficiente.
+            menos tempo, é sobre foco, organização e fluxo contínuo de trabalho.
+            O TaskFlow foi criado para transformar sua rotina com um sistema
+            visual de Kanban intuitivo, flexível e eficiente.
           </p>
         </div>
         <div className="flex-1 flex items-center justify-center">
           <img
             src={kanbanImage}
             alt="Kanban"
-            className="max-w-xs w-full rounded-xl shadow-lg"
+            className="max-w-xs w-full rounded-xl"
           />
         </div>
       </div>
