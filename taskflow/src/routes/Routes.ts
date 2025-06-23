fe330837fa300b0f491828abbfaac8eafa7ceaa7
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-23 01:00:28 -0300

    feat: adding modern routing from react-router-dom

---
+++ taskflow/src/routes/Routes.ts
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+// import App from "@/App";
+// import About from "@/pages/About";
+// import Group from "@/pages/Group";
+// import Home from "@/pages/Home";
+// import Kanban from "@/pages/Kanban";
+// import Projects from "@/pages/Projects";
+
+export const router = createBrowserRouter([]);
