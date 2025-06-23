69f418892b4bcf28ca5d104c5d5a177d84c9f52c
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-23 14:29:16 -0300

    feat(header): add header navigation for routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,28 @@
+import "@/index.css";
+import { useNavigate } from "react-router";
+
 export default function Header() {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate("home");
+  };
+
+  const goToTutorial = () => {
+    navigate("tutorial");
+  };
+
+  const goToProjects = () => {
+    navigate("projects");
+  };
+
   return (
     <header>
       <nav>
         <ul>
-          <li>Inicio</li>
-          <li>Sobre</li>
-          <li>Projetos</li>
-          <li>
-            <img src="" alt="" />
-          </li>
+          <li onClick={goToHome}>Home</li>
+          <li onClick={goToTutorial}>Tutorial</li>
+          <li onClick={goToProjects}>Projetos</li>
         </ul>
       </nav>
     </header>
