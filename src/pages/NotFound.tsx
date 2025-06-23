681efd6cace8fbb64e15b41ce431b8e6b953c005
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-23 12:58:19 -0300

    chore(not-found): fix text

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
   };
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center text-5xl gap-5 bg-gradient-to-t from-red-700 to-red-500">
-      <h1 className="text-white font-bold">Pagina não encontrada !</h1>
+      <h1 className="text-white font-bold">Página não encontrada !</h1>
       <Button onClick={goToLogin} className="cursor-pointer">
         Voltar
       </Button>
