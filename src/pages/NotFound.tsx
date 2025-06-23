c8edd9635d642e2e9fd4015caed6a69bcb887616
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-23 12:56:19 -0300

    feat(not-found): create page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+
 export default function NotFound() {
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate("/");
+  };
   return (
-    <>
-      <h1>Pagina não encontrada!</h1>
-    </>
+    <main className="w-screen h-screen flex flex-col justify-center items-center text-5xl gap-5 bg-gradient-to-t from-red-700 to-red-500">
+      <h1 className="text-white font-bold">Pagina não encontrada !</h1>
+      <Button onClick={goToLogin} className="cursor-pointer">
+        Voltar
+      </Button>
+    </main>
   );
 }
