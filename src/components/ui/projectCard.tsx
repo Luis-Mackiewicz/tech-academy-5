e31747f982b2bf194edcdcc591c23f4fae352f97
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-24 23:30:14 -0300

    feat(ProjectCard): add edit and delete buttons with corresponding handlers

--- src/components/ui/projectCard.tsx
+++ src/components/ui/projectCard.tsx
@@ -10,15 +10,27 @@ type ProjectCardProps = {
   nome: string;
   descricao: string;
   imagem: string;
+  onEdit?: () => void;
+  onDelete?: () => void;
 };
 
 export default function ProjectCard({
   nome,
   descricao,
   imagem,
+  onEdit,
+  onDelete,
 }: ProjectCardProps) {
   return (
-    <Card className="w-64 h-80 flex flex-col justify-between shadow-lg border border-gray-300 bg-white rounded-xl">
+    <Card className="w-64 h-80 flex flex-col justify-between shadow-lg border border-gray-300 bg-white rounded-xl relative">
+      <div className="absolute top-2 right-2 flex gap-2">
+        <button onClick={onEdit} title="Editar">
+          <img src="/public/edit.svg" alt="editar" className="w-5 h-5" />
+        </button>
+        <button onClick={onDelete} title="Excluir">
+          <img src="/public/bin.svg" alt="excluir" className="w-5 h-5" />
+        </button>
+      </div>
       <CardHeader>
         <CardTitle className="text-lg font-bold">{nome}</CardTitle>
       </CardHeader>
